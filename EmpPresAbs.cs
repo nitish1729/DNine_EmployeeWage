26a37323737a7b38bd167c28655a90fe0cab07e3
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-10 09:18:13 +0530

    EmpPresOrAbs

---
+++ EmpPresAbs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNine_EmployeeWage
+{
+    public class EmpPresAbs
+    {
+        int present = 1;
+        void display()
+        {
+			Random ran = new Random();
+			int AttenCheck = ran.Next(0, 2);
+			if (AttenCheck == present) 
+			{
+				Console.WriteLine("Emp is Present");
+			}
+			else
+			{
+				Console.WriteLine("Emp is Absent");
+			}
+		}
+		public static void Main()
+		{
+			EmpPresAbs check = new EmpPresAbs();
+			check.display();
+		}
+
+	}
+    
+}
