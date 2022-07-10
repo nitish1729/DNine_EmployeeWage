c2c300d78396f9482bc24d96e08733f9aaaeea51
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-10 11:37:31 +0530

    Deleted

--- Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("Hello World !!");
-Console.WriteLine(".....................");
-string wellcome;
-wellcome = "Welcome to Employee Wage Computation";
-
-
-    Console.WriteLine(wellcome);
