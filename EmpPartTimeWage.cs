710d41193183398d0671bce83a0b7c0da543e1f7
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-10 10:06:35 +0530

    EmpPartTimeWage

---
+++ EmpPartTimeWage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNine_EmployeeWage
+{
+    public class EmpPartTimeWage
+    {
+        int WagePerHrs = 20;
+        int FullTime = 8;
+        int partTime = 4;
+        int NoWork = 0;
+
+        public void display() // non- static method
+
+        {
+
+            Random ran = new Random();
+
+            int AttenCheck = ran.Next(0, 9);
+
+            if (AttenCheck == FullTime)
+            {
+                Console.WriteLine("Emp is Present");
+                Console.WriteLine("Employee fulltime wage is :- " + (WagePerHrs * FullTime));
+            }
+
+            else if (AttenCheck == partTime)
+            {
+                Console.WriteLine("Emp is present for HalfDay");
+                Console.WriteLine("Employee part time wage is :- " + (WagePerHrs * partTime));
+            }
+
+            else
+            {
+                Console.WriteLine("Emp is Absent");
+                Console.WriteLine("EmpWage : " + (WagePerHrs * NoWork));
+            }
+        }
+        public static void Main(string[] args)
+        {
+            EmpPartTimeWage wages = new EmpPartTimeWage();
+            wages.display();
+        }
+    }
+}
